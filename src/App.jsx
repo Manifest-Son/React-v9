442e6e8dbaca47088003ecc52a9f99333e7f2e78
Manifest-Son <lennoxgithinji37@gmail.com>
2024-12-18 18:31:16 +0300

    This finishes to configure the server.

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,10 @@ const App = () => {
   return (
     <div>
       <h1>Padre Gino's - Order Now</h1>
-      <Pizza name="Pepparoni" description="pep, cheese, n stuff" />
-      <Pizza name="Hawaiiann" description="pep, pinapple, n stuff" />
-      <Pizza name="Americano" description="fresh fries and Dogs" />
-      <Pizza name="Pepparoni" description="pep, cheese, n stuff" />
+      <Pizza name="Pepparoni" description="pep, cheese, n stuff" image={"/public/pizzas/pepperoni.webp"}/>
+      <Pizza name="Hawaiiann" description="pep, pinapple, n stuff" image={"/public/pizzas/hawaiian.webp"}/>
+      <Pizza name="Americano" description="fresh fries and Dogs" image={"/public/pizzas/big_meat.webp"} />
+      <Pizza name="Pepparoni" description="pep, cheese, n stuff" image={"/public/pizzas/sicilian.webp"}/>
     </div>
   )
 };
