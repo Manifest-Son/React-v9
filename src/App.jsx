b8f7861599dffbe7bd14a65536b5c2f75882480e
Manifest-Son <lennoxgithinji37@gmail.com>
2024-12-18 19:38:21 +0300

    This adds hooks in the project.

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
-import Pizza from "./Pizza";
+// import Pizza from "./Pizza";
 import { createRoot } from "react-dom/client";
-
+import Order from "./Order";
 
 
 const App = () => {
   return (
     <div>
       <h1>Padre Gino's - Order Now</h1>
-      <Pizza name="Pepparoni" description="pep, cheese, n stuff" image={"/public/pizzas/pepperoni.webp"}/>
-      <Pizza name="Hawaiiann" description="pep, pinapple, n stuff" image={"/public/pizzas/hawaiian.webp"}/>
-      <Pizza name="Americano" description="fresh fries and Dogs" image={"/public/pizzas/big_meat.webp"} />
-      <Pizza name="Pepparoni" description="pep, cheese, n stuff" image={"/public/pizzas/sicilian.webp"}/>
+    <Order />
     </div>
-  )
+  );
 };
 
 const container = document.getElementById("root");
